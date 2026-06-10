22283a789c6cb014cbc5453c56bc641e65fe7cb5
mf <1251223@isep.ipp.pt>
2026-06-10 15:35:03 +0100

    Cinema.h Cinema.cpp CMakeLists.txt

--- Project/headers/model/Cinema.h
+++ Project/headers/model/Cinema.h
@@ -2,12 +2,21 @@
 #define CINEMA_H
 
 #include "User.h"
+#include "Movie.h"
+#include <vector>
 
 class Cinema {
+private:
+    std::vector<Movie> movies;
+
 public:
     Cinema();
 
     void seedData();
+
+    const std::vector<Movie>& getMovies() const;
+    Movie* getMovieById(int movieId);
+
     void listReservationsByUser(User* user) const;
 };
 
--- Project/sources/model/Cinema.cpp
+++ Project/sources/model/Cinema.cpp
@@ -1,12 +1,28 @@
 #include "Cinema.h"
+#include "Exceptions.h"
 #include <iostream>
 
 Cinema::Cinema() {
 }
 
 void Cinema::seedData() {
-    // function that will create movies, sessions and seats.
+    movies.push_back(Movie(1, "Interstellar", 169, "Sci-Fi"));
+    movies.push_back(Movie(2, "Project Hail Mary", 156, "Sci-Fi"));
+    movies.push_back(Movie(3, "The Notebook", 121, "Drama"));
+}
 
+const std::vector<Movie>& Cinema::getMovies() const {
+    return movies;
+}
+
+Movie* Cinema::getMovieById(int movieId) {
+    for (Movie& movie : movies) {
+        if (movie == movieId) {
+            return &movie;
+        }
+    }
+
+    throw NotFoundException("Movie not found");
 }
 
 void Cinema::listReservationsByUser(User* user) const {
