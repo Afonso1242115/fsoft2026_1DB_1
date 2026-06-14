5353d8f1a9aa5719f3db5877ccb6f3b77581047e
Afonso1242115 <1242115@isep.ipp.pt>
2026-06-14 12:54:20 +0100

    *Adding Content to Mapper

--- Project/sources/mapper/SeatMapper.cpp
+++ Project/sources/mapper/SeatMapper.cpp
@@ -1,3 +1,11 @@
-//
-// Created by Afonso on 11/06/2026.
-//
+#include "SeatMapper.h"
+
+SeatDTO SeatMapper::modelToDTO(const Seat& seat) {
+    return SeatDTO(seat.getCode(), seat.isReserved());
+}
+
+std::vector<SeatDTO> SeatMapper::listToDTO(const std::vector<Seat>& seats) {
+    std::vector<SeatDTO> dtos;
+    for (const Seat& seat : seats) dtos.push_back(modelToDTO(seat));
+    return dtos;
+}
